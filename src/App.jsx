1da9e0765e19f99df55abbfeeaaa2918ad9b67e0
Julia <135132600+AlexsandrKotobus@users.noreply.github.com>
2024-05-06 16:22:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './assets/styles/global.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='car-item'>
+        <h1>Cars catalog</h1>
+        <h2>Car 1</h2>
+        <p>$ 100 000</p>
+        <button>Read more</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
